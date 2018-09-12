4641044f23cae68cdc3fcb36c3e12cf909208d67
Stephen Boyle <stephen@thirdvantage.com>
2018-09-12 16:19:43 -0400

    Move to munkipkg

--- check_folder_sizes/_check_folder_sizes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-output="/Library/MonitoringClient/PluginSupport/_check_folder_sizes.txt"
-
-echo -e "Largest 25 folders as of $(date):\n<tt>" > "${output}"
-nice -n 20 du -kh /Library /Applications /Users /var | sort -rh | grep '[0-9][G,T]\>\t' | head -25 >> "${output}"
-echo -e "</tt>" >> "${output}"
