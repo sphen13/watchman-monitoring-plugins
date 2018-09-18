0cd75f99fdb21eb49b76f9228cf1c577d4395580
Stephen Boyle <stephen@thirdvantage.com>
2018-09-18 18:25:36 -0400

    fixes

--- _check_folder_sizes/payload/Library/MonitoringClient/PluginSupport/_check_folder_sizes.sh
+++ _check_folder_sizes/payload/Library/MonitoringClient/PluginSupport/_check_folder_sizes.sh
@@ -2,7 +2,7 @@
 
 output="/Library/MonitoringClient/PluginSupport/_check_folder_sizes.txt"
 
-echo -e "Largest 25 folders as of $(date):\n<tt>" > "${output}"
+echo "Largest 25 folders over 5GB as of $(date):\n<tt>" > "${output}"
 nice -n 20 du -k /Library /Applications /Users /var | awk '$1 > 5242880 { print }' | sort -nr | head -25 | awk '
     function human(x) {
         if (x<1000) {return x} else {x/=1024}
@@ -12,4 +12,4 @@ nice -n 20 du -k /Library /Applications /Users /var | awk '$1 > 5242880 { print 
         return int(x+0.5) substr(s,1,1)
     }
     {sub(/^[0-9]+/, human($1)); print}' >> "${output}"
-echo -e "</tt>" >> "${output}"
+echo "</tt>" >> "${output}"
